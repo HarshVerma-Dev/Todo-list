564e36aa12059b2ed8683687865620fe5cf516d1
HarshVerma-Dev <harshverma9984402466@gmail.com>
2026-07-13 15:56:19 +0530

    Enhance todo list with input validation and button toggle
    Refactor todo list functionality to include input validation and toggle button state.

--- index.js
+++ index.js
@@ -1,40 +1,72 @@
-let todolist = [{
-        item: 'Buy milk',
-        dueDate: '25/05/26'
-    },
-    {
-        item: 'go to work',
-        dueDate: '25/05/26'
-    }
-];
+let todolist = [];
+
 displayItems();
 
-function addTodo() {
-    let inputElement = document.querySelector('#todo-input');
-    let dateElement = document.querySelector('#todo-date');
-    let todoItem = inputElement.value;
-    let todoDate = dateElement.value;
-    todolist.push({ item: todoItem, dueDate: todoDate });
-    inputElement.value = '';
-    dateElement.value = '';
+// Enable/Disable Add Button
+function toggleButton() {
+    let taskInput = document.querySelector("#todo-input");
+    let addButton = document.querySelector("#add-btn");
 
+    if (taskInput.value.trim() === "") {
+        addButton.disabled = true;
+    } else {
+        addButton.disabled = false;
+    }
+}
+
+// Add Todo
+function addTodo() {
+    let inputElement = document.querySelector("#todo-input");
+    let dateElement = document.querySelector("#todo-date");
+
+    let todoItem = inputElement.value.trim();
+    let todoDate = dateElement.value;
+
+    // Prevent empty task
+    if (todoItem === "") {
+        return;
+    }
+
+    // Add task
+    todolist.push({
+        item: todoItem,
+        dueDate: todoDate
+    });
+
+    // Clear inputs
+    inputElement.value = "";
+    dateElement.value = "";
+
+    // Disable button again
+    toggleButton();
+
+    // Refresh list
     displayItems();
 }
 
+// Display Todo List
 function displayItems() {
-    let containerElement = document.querySelector('.todo-container');
-    let newHtml = '';
+    let containerElement = document.querySelector(".todo-container");
+    let newHtml = "";
+
     for (let i = 0; i < todolist.length; i++) {
-        // let item = todolist[i].item;
-        // let dueDate = todolist[i].dueDate;
+
         let { item, dueDate } = todolist[i];
+
         newHtml += `
-       
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button class='btn-delete' onclick="todolist.splice(${i}, 1);
-        displayItems();">Delete</button>
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button class="btn-delete" onclick="deleteTodo(${i})">
+                Delete
+            </button>
         `;
     }
+
     containerElement.innerHTML = newHtml;
+}
+
+// Delete Todo
+function deleteTodo(index) {
+    todolist.splice(index, 1);
+    displayItems();
 }
